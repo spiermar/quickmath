6ddb322ad1d33450feb3b6583e861ef66c843752
Martin Spier <spiermar@gmail.com>
2020-09-26 16:04:16 -0700

    refactor: better design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,41 +1,47 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import TypoGraphy from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const styles = {
-  headerText: {
+  logoContainer: {
     textAlign: 'center',
-    marginBottom: 50,
   },
   lessonButton: {
-    margin: 'auto',
     display: 'block',
-    marginBottom: 10,
+    margin: 'auto',
+    marginBottom: 15,
+    backgroundColor: "#70C6C7",
+    color: "#F1FAEE",
+  },
+  buttons: {
   }
 }
 
 function Home() {
   return (
     <div>
-      <TypoGraphy style={styles.headerText} variant="h3" color="inherit">Quick Math</TypoGraphy>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Double Digit Sum
-      </Button>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Triple Digit Sum
-      </Button>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Double Digit Sub
-      </Button>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Triple Digit Sub
-      </Button>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Single Digit Multi
-      </Button>
-      <Button style={styles.lessonButton} variant="contained" color="primary">
-        Double Digit Multi
-      </Button>
+      <div style={styles.logoContainer}>
+        <img alt="logo" src="logo.png" height="256px" />
+      </div>
+      <div style={styles.buttons}>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Double Digit Sum
+        </Button>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Triple Digit Sum
+        </Button>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Double Digit Sub
+        </Button>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Triple Digit Sub
+        </Button>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Single Digit Multi
+        </Button>
+        <Button style={styles.lessonButton} variant="contained" color="primary">
+          Double Digit Multi
+        </Button>
+      </div>
     </div>
   );
 }
