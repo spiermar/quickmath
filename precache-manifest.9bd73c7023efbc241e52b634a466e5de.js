b3d378df39d2b70132bde8459dae228fe9eaeaab
Martin Spier <spiermar@gmail.com>
2020-09-27 11:35:09 -0700

    Intial commit

---
+++ precache-manifest.9bd73c7023efbc241e52b634a466e5de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79405025830e12560659cc0a7d8f79cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c417711bde16c5c9eb16",
+    "url": "/static/css/main.8fe47591.chunk.css"
+  },
+  {
+    "revision": "80b2b168b39eb581e845",
+    "url": "/static/js/2.22cb944e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.22cb944e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c417711bde16c5c9eb16",
+    "url": "/static/js/main.1190fb07.chunk.js"
+  },
+  {
+    "revision": "ab7153ef6d0336dc3711",
+    "url": "/static/js/runtime-main.f99fe41c.js"
+  }
+]);
