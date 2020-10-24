fbd1c9c5465e00d085c14fdbc0adaf9c882d12f9
Martin Spier <spiermar@gmail.com>
2020-10-24 12:03:16 -0700

    feat: lesson style, next and home actions

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,23 +3,29 @@ import Button from '@material-ui/core/Button';
 import { useHistory } from "react-router-dom";
 
 const styles = {
-  logoContainer: {
-    height: 256,
-    textAlign: 'center',
-  },
-  lessonButton: {
-    display: 'block',
-    margin: 'auto',
-    marginBottom: 15,
-  },
-  buttonContainer: {
-    margin: 'auto',
-  },
-  container: {
-    height: 'calc(100vh - 272px)',
-    display: 'flex',
-    alignItems: 'center',
-  }
+    logoContainer: {
+        height: 256,
+        textAlign: 'center',
+    },
+    lessonButton: {
+        display: 'block',
+        margin: 'auto',
+        marginBottom: 15,
+        backgroundColor: '#70C6C7',
+        color: '#F1FAEE',
+    },
+    disabledLessonButton: {
+        backgroundColor: '#DCF1E8',
+        color: '#86CFCE',
+    },
+    buttonContainer: {
+        margin: 'auto',
+    },
+    container: {
+        height: 'calc(100vh - 272px)',
+        display: 'flex',
+        alignItems: 'center',
+    }
 }
 
 function Home() {
@@ -35,19 +41,19 @@ function Home() {
           <Button style={styles.lessonButton} variant="contained" size="large" color="primary" onClick={() => { history.push("/doubledigitsum") }}>
             Double Digit Sum
           </Button>
-          <Button disabled style={styles.lessonButton } variant="contained" size="large" color="primary">
+          <Button disabled style={{ ...styles.lessonButton, ...styles.disabledLessonButton}} variant="contained" size="large" color="primary">
             Triple Digit Sum
           </Button>
-          <Button disabled style={styles.lessonButton} variant="contained" size="large" color="primary">
+          <Button disabled style={{ ...styles.lessonButton, ...styles.disabledLessonButton}} variant="contained" size="large" color="primary">
             Double Digit Sub
           </Button>
-          <Button disabled style={styles.lessonButton} variant="contained" size="large" color="primary">
+          <Button disabled style={{ ...styles.lessonButton, ...styles.disabledLessonButton}} variant="contained" size="large" color="primary">
             Triple Digit Sub
           </Button>
-          <Button disabled style={styles.lessonButton} variant="contained" size="large" color="primary">
+          <Button disabled style={{ ...styles.lessonButton, ...styles.disabledLessonButton}} variant="contained" size="large" color="primary">
             Single Digit Multi
           </Button>
-          <Button disabled style={styles.lessonButton} variant="contained" size="large" color="primary">
+          <Button disabled style={{ ...styles.lessonButton, ...styles.disabledLessonButton}} variant="contained" size="large" color="primary">
             Double Digit Multi
           </Button>
         </div>
--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router-dom';
 
-const useStyles = makeStyles({
+const styles = {
     card: {
-      color: '#F1FAEE',
-      backgroundColor: '#70C6C7',
-      minWidth: 275,
-      margin: 'auto',
+        color: '#F1FAEE',
+        backgroundColor: '#70C6C7',
+        minWidth: 275,
+        margin: 'auto',
     },
     cardContent: {
         textAlign: 'center',
@@ -31,37 +33,74 @@ const useStyles = makeStyles({
     },
     answer: {
         marginTop: 25,
-        marginBottom: 25,
+        marginBottom: 45,
         display: 'block',
+    },
+    nextButton: {
+        display: 'block',
+        margin: 'auto',
+        fontSize: 32,
+        marginBottom: 15,
+        backgroundColor: '#DCF1E8',
+        color: '#86CFCE',
+    },
+    homeButton: {
+        position: 'fixed',
+        top: 25,
+        left: 25,
+        backgroundColor: '#70C6C7',
+        color: '#F1FAEE',
     }
-});
+};
 
+class Lesson extends React.Component {
 
-function Lesson() {
-    const classes = useStyles();
-    const firstNumber = 56;
-    const secondNumber = 21;
+    constructor(props){
+        super(props);
+        this.state = {
+            firstNumber: 56,
+            secondNumber: 21,
+        }
+    }
 
-    return (
-        <div>
-            <div className={classes.container}>
-                <Card className={classes.card}>
-                    <CardContent className={classes.cardContent}>
-                        <Typography className={classes.number} variant="h2" gutterBottom>
-                            {firstNumber}
-                        </Typography>
-                        <Typography className={classes.operand} variant="h3" gutterBottom>
-                            +
-                        </Typography>
-                        <Typography className={classes.number} variant="h2" gutterBottom>
-                            {secondNumber}
-                        </Typography>
-                        <TextField className={classes.answer} variant="outlined" />
-                    </CardContent>
-                </Card>
+    handleNextClick = () => {
+        console.log("next")
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={styles.container}>
+                    <Button style={styles.homeButton} variant="contained" size="large" color="primary" onClick={() => { this.props.history.push("/") }}>
+                        Home
+                    </Button>
+                    <Card style={styles.card}>
+                        <CardContent style={styles.cardContent}>
+                            <Typography style={styles.number} variant="h2" gutterBottom>
+                                {this.state.firstNumber}
+                            </Typography>
+                            <Typography style={styles.operand} variant="h3" gutterBottom>
+                                +
+                            </Typography>
+                            <Typography style={styles.number} variant="h2" gutterBottom>
+                                {this.state.secondNumber}
+                            </Typography>
+                            <TextField style={styles.answer} variant="outlined" type="number" />
+                            <Button style={styles.nextButton} variant="contained" size="large" color="primary" onClick={this.handleNextClick}>
+                                Next
+                            </Button>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Lesson;
+Lesson.propTypes = {
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired,
+    }),
+}
+
+export default withRouter(Lesson);
